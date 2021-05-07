dce816d6bc36b8a2b9d274799c3db0d088a7a608
InnMish <mishustina12@inbox.ru>
2021-05-07 14:03:44 +0300

    dependency injection without Spring

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+
+    private final GreetingService service;
+
+    public ConstructorInjectedController(GreetingService service) {
+        this.service = service;
+    }
+
+    public String sayGreeting() {
+        return service.sayGreeting();
+    }
+}
